a338b6c7a8ab6bdee120ae6d5bc99a92a3308a93
katya <katya.com>
2024-05-05 13:25:56 +0600

    commit

--- src/components/appBar/primarySearchAppBar.js
+++ src/components/appBar/primarySearchAppBar.js
@@ -182,18 +182,18 @@ export default function PrimarySearchAppBar() {
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
             <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="error">
+              {/* <Badge badgeContent={4} color="error"> */}
                 <MailIcon />
-              </Badge>
+              {/* </Badge> */}
             </IconButton>
             <IconButton
               size="large"
               aria-label="show 17 new notifications"
               color="inherit"
             >
-              <Badge badgeContent={17} color="error">
+              {/* <Badge badgeContent={17} color="error"> */}
                 <NotificationsIcon />
-              </Badge>
+              {/* </Badge> */}
             </IconButton>
             <IconButton
               size="large"
